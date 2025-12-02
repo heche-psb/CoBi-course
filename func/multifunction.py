031abd43d0cd2fb18502a9bdb8cf4fcebdadefec
heche-psb <heche@HengchiChen>
2025-12-02 16:24:16 +0100

    Update func

--- func/multifunction.py
+++ func/multifunction.py
@@ -181,5 +181,45 @@ def renamegenetree(treefile,gsmap,output):
         tip.name = Gsmap[tip.name]
     Phylo.write(tree,output,format='newick')
 
+def plotlinearregression(X,Y,output):
+    data_xy = [(x,y) for x,y in zip(X,Y)]
+    data_xy = sorted(data_xy, key=lambda x:x[0])
+    sorted_X,sorted_Y = np.array([x for x,y in data_xy]),np.array([y for x,y in data_xy])
+    fig, ax = plt.subplots(1,1,figsize=(4, 4))
+    ax.scatter(sorted_X,sorted_Y,color='k',label='Original data points',alpha=0.5,zorder=1)
+    ax.set_xlabel("sCF",fontsize=10)
+    ax.set_ylabel("gCF",fontsize=10)
+    slope, intercept, r_value, p_value, std_err = stats.linregress(sorted_X,sorted_Y)
+    y_fit = intercept + slope * sorted_X
+    ax.plot(sorted_X, y_fit, color='k', label='Fitted linear function',zorder=2)
+    se = np.sqrt(np.sum((sorted_Y - y_fit)**2) / (len(sorted_Y) - 2))
+    t_value = stats.t.ppf(1 - 0.025, df=len(sorted_X) - 2)
+    ci = t_value * se * np.sqrt(1/len(sorted_X) + (sorted_X - np.mean(sorted_X))**2 / np.sum((sorted_X - np.mean(sorted_X))**2))
+    ax.fill_between(sorted_X, y_fit-ci, y_fit+ci, color='gray', alpha=0.5, label='95% confidence band',zorder=3)
+    ax.plot([],[],color='white',label='R-squared: {:.4f}'.format(r_value**2))
+    ax.plot([],[],color='white',label='Slope: {:.4f}'.format(slope))
+    ax.plot([],[],color='white',label='P-value: {:.4f}'.format(p_value))
+    order_list = ['Fitted linear function','95% confidence band','Original data points','R-squared: {:.4f}'.format(r_value**2),'Slope: {:.4f}'.format(slope),'P-value: {:.4f}'.format(p_value)]
+    handles, labels = ax.get_legend_handles_labels()
+    labels, handles = zip(*sorted(zip(labels, handles), key=lambda x: order_list.index(x[0])))
+    ax.legend(handles,labels,fontsize=10,loc=0,frameon=False)
+    fig.tight_layout()
+    fig.savefig(output,format ='svg', bbox_inches='tight')
+    plt.close()
+
+@cli.command(context_settings={'help_option_names': ['-h', '--help']})
+@click.argument('scffile', type=click.Path(exists=True))
+@click.argument('gcffile', type=click.Path(exists=True))
+@click.option('--output', '-o', default=None, show_default=True, help='output filename')
+def scfgcfconstrast(scffile,gcffile,output):
+    """
+    compare scf with gcf
+    """
+    df_gcf = pd.read_csv(gcffile,skiprows=17,header=0,index_col=None,sep='\t')
+    df_scf = pd.read_csv(scffile,skiprows=14,header=0,index_col=None,sep='\t')
+    gcf = df_gcf['gCF'].to_list()
+    scf = df_scf['sCF'].to_list()
+    plotlinearregression(scf,gcf,output)
+
 if __name__ == '__main__':
 	cli()
